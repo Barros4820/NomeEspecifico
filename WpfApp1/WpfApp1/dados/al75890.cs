979ae670634f37c699c13df06b120e2096ffcc3c
root <claudio.m.macedo32@gmail.com>
2023-03-15 12:41:12 +0000

    claudio class

---
+++ WpfApp1/WpfApp1/dados/al75890.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.dados
+{
+    internal class al75890
+    {
+    }
+}
